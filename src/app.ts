33b07e97c1ee9abd59cf652eea155e94c84490b0
Julio Cesar <jcmljunior@proton.me>
2025-03-13 13:01:33 -0300

    Adicionado configurações do pacote cors.

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express from 'express';
+import cors from "cors";
 import accountRoutes from "./routes/accountRoutes";
 
 const app = express();
-
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use('/account/v1', accountRoutes);
-
 app.listen(process.env.EXPRESS_PORT || 3000, () => {
   console.log(`🚀 Servidor rodando na porta ${process.env.EXPRESS_PORT || 3000}`);
 });
